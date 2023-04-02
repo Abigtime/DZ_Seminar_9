201a0de900f52ac84dc621242ed176a9c461ec87
Abigtime <115564855+Abigtime@users.noreply.github.com>
2023-04-02 20:53:09 +0300

    код программы

--- DZ_65/Program.cs
+++ DZ_65/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        int M = 3;
+        int N = 1;
+        int sum = 0;
+
+        // Если M > N, меняем их значения местами
+        if (M > N) {
+            int temp = M;
+            M = N;
+            N = temp;
+        }
+
+        // Вычисляем сумму натуральных чисел от M до N
+        for (int i = M; i <= N; i++) {
+            sum += i;
+        }
+
+        Console.WriteLine($"Сумма натуральных чисел от {M} до {N} равна {sum}");
+    }
+}
