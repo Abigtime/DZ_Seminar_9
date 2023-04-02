962963fae36ebb84d1560600043dd52e1e787070
Abigtime <115564855+Abigtime@users.noreply.github.com>
2023-04-02 20:52:09 +0300

    код программы

--- DZ_64/Program.cs
+++ DZ_64/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// N = 3 программа выведит: 3, 2, 1 ; N = 8 программа выдаст: 8, 7, 6, 5, 4, 3, 2, 1.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = 8;
+        PrintNumbers(n);
+    }
+
+    static void PrintNumbers(int n)
+    {
+        if (n == 1)
+        {
+            Console.Write(n);
+            return;
+        }
+
+        Console.Write(n + ", ");
+        PrintNumbers(n - 1);
+    }
+}
+
